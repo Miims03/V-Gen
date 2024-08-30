0d5d7bca43b477beddd8f560b10b5d2b12afe7d0
Mims <amine.chr2@gmail.com>
2024-08-30 16:53:36 +0200

    evol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component , AfterViewInit ,HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./layouts/navbar/navbar.component";
-import { ViewportScroller } from '@angular/common';
-import { Router, NavigationEnd } from '@angular/router';
-import 'animate.css';
+import { Component , AfterViewInit ,HostListener } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from "./layouts/navbar/navbar.component"
+import { ViewportScroller } from '@angular/common'
+import { Router, NavigationEnd } from '@angular/router'
+import 'animate.css'
 
 
 @Component({
@@ -13,52 +13,41 @@ import 'animate.css';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements AfterViewInit {
-  private currentSectionIndex = 0;
-  // private isScrolling = false;
+  private currentSectionIndex = 0
 
   ngAfterViewInit() {
-    // Les sections sont déjà définies avec des IDs dans le HTML, donc aucune action supplémentaire n'est nécessaire ici.
   }
 
   @HostListener('window:wheel', ['$event'])
   onScroll(event: WheelEvent) {
-    // if (this.isScrolling) {
-    //   return; 
-    // }
-
-    // this.isScrolling = true;
 
     if (event.deltaY > 0) {
-      this.goToNextSection(); // Scroll vers le bas
+      this.goToNextSection()
     } else if (event.deltaY < 0) {
-      this.goToPreviousSection(); // Scroll vers le haut
+      this.goToPreviousSection()
     }
-
-    // setTimeout(() => {
-    //   this.isScrolling = false;
-    // }, 500);
   }
 
   private goToNextSection() {
-    const sections = ['home', 'about', 'produits'];  // IDs des sections dans l'ordre
+    const sections = ['home', 'about', 'produits']
     if (this.currentSectionIndex < sections.length - 1) {
-      this.currentSectionIndex++;
-      this.scrollToSection(sections[this.currentSectionIndex]);
+      this.currentSectionIndex++
+      this.scrollToSection(sections[this.currentSectionIndex])
     }
   }
 
   private goToPreviousSection() {
-    const sections = ['home', 'about', 'produits'];  // IDs des sections dans l'ordre
+    const sections = ['home', 'about', 'produits']
     if (this.currentSectionIndex > 0) {
-      this.currentSectionIndex--;
-      this.scrollToSection(sections[this.currentSectionIndex]);
+      this.currentSectionIndex--
+      this.scrollToSection(sections[this.currentSectionIndex])
     }
   }
 
   private scrollToSection(sectionId: string) {
-    const element = document.getElementById(sectionId);
+    const element = document.getElementById(sectionId)
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });  // Défilement fluide
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
     }
   }
 }
